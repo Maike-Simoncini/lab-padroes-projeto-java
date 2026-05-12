06550e30c26c279fce66b1ded66adb3452aa8397
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2026-05-12 00:05:52 -0300

    Create TratamentoOcorrenciaStrategy interface

---
+++ src/main/java/me/dio/labpadroesprojeto/service/TratamentoOcorrenciaStrategy.java
@@ -0,0 +1,6 @@
+package me.dio.labpadroesprojeto.service;
+import me.dio.labpadroesprojeto.model.Ocorrencia;
+
+public interface TratamentoOcorrenciaStrategy {
+    void tratar(Ocorrencia ocorrencia);
+}
