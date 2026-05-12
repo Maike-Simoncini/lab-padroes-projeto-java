56618a796b923512bff3ae629c29e915e3a8b5b4
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2026-05-11 23:59:41 -0300

    Create EnderecoRepository interface for Endereco model

---
+++ (src/main/java/me/dio/labpadroesprojeto/repository/​EnderecoRepository.java
@@ -0,0 +1,5 @@
+// EnderecoRepository.java
+package me.dio.labpadroesprojeto.repository;
+import org.springframework.data.repository.CrudRepository;
+import me.dio.labpadroesprojeto.model.Endereco;
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {}
