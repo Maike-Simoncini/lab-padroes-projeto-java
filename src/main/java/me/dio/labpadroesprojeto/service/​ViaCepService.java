9c5845b331888c7459843284ec443768a24bd8da
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2026-05-12 00:04:25 -0300

    Add ViaCepService interface for address lookup

---
+++ src/main/java/me/dio/labpadroesprojeto/service/​ViaCepService.java
@@ -0,0 +1,12 @@
+package me.dio.labpadroesprojeto.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import me.dio.labpadroesprojeto.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
